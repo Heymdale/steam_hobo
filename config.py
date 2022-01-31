5b4d0e7d96f34bf2b7ba293bbf1d133b9fe95c61
Heymdale <sivolo4@yandex.ru>
2022-01-31 15:29:40 +0400

    0.3.1 with concurrent steam execution but still bugged version

--- config.py
+++ config.py
@@ -13,10 +13,11 @@
 class Users:
     # Write the tuple of this linux OS users, which will run steam
     # 0 index must contain name of main user, which is in sudoers, have NOPASSWD and from whom we start script
-    linux_users_list = ('heymdale',
+    linux_users_list = ('cat',
+                        'viktor',
+                        'heymdale',
                         'gyal',
-                        'cat',
-                        'viktor')
+                        )
 
 
 # Headline of warning window in your Linux OS
--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import subprocess as sp
 import config
 import pyautogui as pag
 from ewmh import EWMH
-from os import environ
 
 
 def logs(*args, to_warn=False):
-    message = str(datetime.datetime.now())
+    message = str(datetime.datetime.now()) + ' '
     for s in args:
         message += str(s)
     if config.debug:
@@ -19,12 +18,20 @@ def logs(*args, to_warn=False):
         # Заглушка, в следующих версиях должна отправлять сообщения по почте или через телеграм бота.
         # Следует определиться на каком языке писать комментарии.
         print('\033[91m', message, '\033[0m')
-    with open('./log', 'w+') as f:  # Прошлая версия была без параметра 'w+', выкидывала ошибку, пытаясь открыть
+    with open('./log', 'a') as f:  # Прошлая версия была без параметра 'w+', выкидывала ошибку, пытаясь открыть
         # несуществующий файл, но эта ошибка нигде не отображалась.
         f.writelines(message)
 
+def find_window_of_user(current_game_windows, dontstarve_windows, concurrent_users_count):
+    for el in dontstarve_windows:
+        for i in range(concurrent_users_count):
+            if not el == current_game_windows[i]:
+                dontstarve_window = el
+                current_game_windows[i] = el
+                return dontstarve_window
 
-def run(user_index, remaining_users_to_work_concurrently):
+
+def run(user_index, concurrent_users_count, current_game_windows):
     user = config.Users.linux_users_list[user_index]
     print('Start ', user)
     #  Запускаем steam и игру
@@ -35,24 +42,14 @@ def run(user_index, remaining_users_to_work_concurrently):
         logs('Ожидание загрузки игры вышло')
         # Закрываем предупреждение, появляющееся, если используем регистронезависимые файловые системы
         close_warning()
-        dontstarve_window = activate_dontstarve_window(user)
+        dontstarve_windows = ewmh_wrapper("Don't Starve Together", strong=True)
+        dontstarve_window = find_window_of_user(current_game_windows, dontstarve_windows, concurrent_users_count)
         logs('Начинаем вводить команды')
         dontstarve_control(dontstarve_window)
         ewmh = EWMH()
         ewmh.setWmState(dontstarve_window, 1, '_NET_WM_STATE_SHADED')
         ewmh.display.flush()
-        logs('Ожидание')
-        if not remaining_users_to_work_concurrently == 1:
-            if len(config.Users.linux_users_list) > user_index+1:
-                run(user_index + 1, remaining_users_to_work_concurrently - 1)
-        for i in range(config.game_time_minutes):
-            # TODO Каждую минуту проверять запущена ли игра
-            if True:
-                time.sleep(60)
-            else:
-                break
-        # stop_steam_game() was moved to the main function
-        logs('finish run')
+        logs('"Играем" отведенное время')
 
 
 def stop_steam_game():
@@ -66,14 +63,14 @@ def share_xserver(users):
 
 
 def close_warning():
-    # TODO ewmh_wrapper изменен придется переписывать функцию
     ewmh = EWMH()
-    ewmh_wrapper(config.warning_headline, ewmh.setCloseWindow)
-    ewmh.display.flush()  # Во wrapper данная команда не работала, т.к. мы не передавали экземпляр EWMH().
-    # Поэтому мы используем flush() после вызова wrapper или передаём переменную ewmh в ewmh_wrapper.
+    warning_windows = ewmh_wrapper(config.warning_headline)
+    for el in warning_windows:
+        ewmh.setCloseWindow(el)
+    ewmh.display.flush()
 
 
-def ewmh_wrapper(substrings, ewmh_func, strong=False, case_insensitive=True):
+def ewmh_wrapper(substrings, strong=True, case_insensitive=False):
     # С добавлением поиска строки целиком, враппер стал уже некрасив.
     # Оказалось, что приписка от какого пользователя запущена игра, не часть заголовка окна,
     # большая часть кода в данной функции написана зря.
@@ -99,7 +96,6 @@ def ewmh_wrapper(substrings, ewmh_func, strong=False, case_insensitive=True):
                     w_name = w_name_binary.decode('utf-8')
                 if w_name == find_name:
                     find_windows.append(el)
-                    break
     else:
         find_windows = windows
         for substr in substrings:
@@ -116,26 +112,18 @@ def ewmh_wrapper(substrings, ewmh_func, strong=False, case_insensitive=True):
                         w_name = w_name_binary.decode('utf-8')
                     if -1 != w_name.find(substr):
                         find_windows.append(el)
-    if len(find_windows) > 0:
-        ewmh_func(find_windows[0])
-        return find_windows[0]
+    return find_windows
 
 
-def activate_dontstarve_window(user):
-    # TODO ewmh_wrapper изменен придется переписывать функцию
+def activate_dontstarve_window(window):
     ewmh = EWMH()
-    if environ.get('USER') == user:
-        dontstarve_window = ewmh_wrapper("Don't Starve Together", ewmh.setActiveWindow, strong=True)
-    else:
-        dontstarve_window = ewmh_wrapper(("Don't Starve Together", user), ewmh.setActiveWindow)
+    ewmh.setActiveWindow(window)
     ewmh.display.flush()
-    return dontstarve_window
+    time.sleep(0.1) # TODO провести тесты, нужна ли задержка и достаточна ли она для фокуса на нужном окне
 
 
-def in_window_key_press(starve_window, cmd: "str pyautogui KEYBOARD_KEYS", pause_in_sec=0.5):
-    ewmh = EWMH()
-    ewmh.setActiveWindow(starve_window)
-    ewmh.display.flush()
+def in_window_key_press(starve_window, cmd: "str pyautogui KEYBOARD_KEYS", pause_in_sec=0.2):
+    activate_dontstarve_window(starve_window)
     pag.press(cmd)
     logs('Pressed ', cmd)
     time.sleep(pause_in_sec)
@@ -164,9 +152,21 @@ def main():
         start_next = start_time + datetime.timedelta(minutes=config.looptime_minutes)
         concurrent_users_count = config.concurrent_users
         concurrent_users_count == concurrent_users_count if concurrent_users_count > 0 else len(users)
-        for i in range(0, len(users)):
-            run(i, config.concurrent_users)
+        i = 0
+        while i < len(users):
+            current_game_windows = [None] * concurrent_users_count
+            for concurrent in range(i, i + concurrent_users_count):
+                if i < len(users):
+                    run(i, concurrent_users_count, current_game_windows)
+            i += concurrent_users_count
+            for i in range(config.game_time_minutes):
+                # TODO Каждую минуту проверять запущена ли игра
+                if True:
+                    time.sleep(60)
+                else:
+                    break
             stop_steam_game()
+            logs('finish run')
         if not config.loop:
             break
         end_time = datetime.datetime.now()
