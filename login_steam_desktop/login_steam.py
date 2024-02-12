dfa52b6be491466e73e605afe9d2d79a1dd02b55
Sergey Bunakov <sivolo4@yandex.ru>
2024-02-13 01:06:50 +0400

    Added foreground window function for LoginUserToSteam

--- login_steam_desktop/login_steam.py
+++ login_steam_desktop/login_steam.py
@@ -23,6 +23,7 @@ class LoginUserToSteam:
             steam_location = steam_location.replace(sep, '/')
         steam_location.replace('/', r'\\')
         self.steam_location = steam_location
+        self.steam_window_manager = None
 
     @staticmethod
     def __stop_steam():
@@ -33,6 +34,12 @@ class LoginUserToSteam:
         sp.Popen([self.steam_location, '-silent', '-login', self.login],
                  stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
 
+    def __click_on_this_window(self):
+        # To foreground window
+        if self.steam_window_manager is not None:
+            self.steam_window_manager.window_to_foreground()
+            pag.click()
+
     def login_steam(self):
         # Stop all instances of steam, except steamservice.exe
         self.__stop_steam()
@@ -41,20 +48,20 @@ class LoginUserToSteam:
         # TODO: Add cycle with check if app has been appeared and exception if not in 5 minutes
         time.sleep(20)
         hwnd_steam = WinMgr.get_hwnd_by_name('Sign in to Steam')
-        steam_window = WinMgr(hwnd_steam)
+        self.steam_window_manager = WinMgr(hwnd_steam)
         print(f'{hwnd_steam=}')
-        r_left, r_top, r_right, r_bottom = steam_window.get_window_rect_from_hwnd()
+        r_left, r_top, r_right, r_bottom = self.steam_window_manager.get_window_rect_from_hwnd()
         r_height = r_bottom - r_top
         r_width = r_right - r_left
         print(f'{r_left=}, {r_top=}, {r_right=}, {r_bottom=}, {r_height=}, {r_width=}')
         if r_left > 0 and r_top > 0:
             point_on_name_field = r_left + r_width/15, r_top + r_height/3
             pag.moveTo(point_on_name_field, duration=0.5)
-            pag.click()
+            self.__click_on_this_window()
             pag.typewrite(self.login)
             point_on_pass_field = r_left + r_width/15, r_top + r_height/2
             pag.moveTo(point_on_pass_field, duration=0.5)
-            pag.click()
+            self.__click_on_this_window()
             pag.typewrite(self.password)
             pag.press('enter')
             # TODO: Add async request 2fa, check if steam-guard widow appear
@@ -63,7 +70,7 @@ class LoginUserToSteam:
             time.sleep(15)
             point_on_totp_field = r_left + r_width/3, r_top + r_height/2
             pag.moveTo(point_on_totp_field, duration=0.5)
-            pag.click()
+            self.__click_on_this_window()
             pag.typewrite(totp_code)
             pag.press('enter')
         else:
