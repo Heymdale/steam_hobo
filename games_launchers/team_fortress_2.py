755c9a8146386ee4784d5fa7be94f5fe8df65c1c
Sergey Bunakov <sivolo4@yandex.ru>
2024-04-27 14:20:39 +0400

    Edit team fortress 2 process name

--- games_launchers/team_fortress_2.py
+++ games_launchers/team_fortress_2.py
@@ -5,7 +5,7 @@ class TeamFortress2(GameLauncher):
     """Stopping with 'stop_if_none' function of GameLauncher class work badly.
     So minimum implement for close game normally.
     Don't forget to add this class and game id to launcher.py"""
-    _process_names = 'hl2.exe'
+    _process_names = 'tf_win64.exe'
 
     def in_game_activity(self):
         ...
