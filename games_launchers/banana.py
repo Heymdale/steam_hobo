85021553187d8a1b976fe0eb9a27db0005861486
Sergey Bunakov <sivolo4@yandex.ru>
2024-06-17 15:01:24 +0400

    Added threading to ingame behavior to Banana (Cats) for adding hotkeys (Enter, Esc) for stop/start clicking.

--- games_launchers/banana.py
+++ games_launchers/banana.py
@@ -66,8 +66,8 @@ class Banana(GameLauncher):
                          args=(center_point,),
                          kwargs={'listener': listener,
                                  'clicks_interval_in_secs': 0.0666,
-                                 'min_click_count': 3500,
-                                 'plus_random_click_count': 1000},
+                                 'min_click_count': 1500,
+                                 'plus_random_click_count': 700},
                          daemon=False).start()
         # With join() main algorithm will wait until listener thread are exit, else it will run further.
         listener.join()
